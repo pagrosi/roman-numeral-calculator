5135e7fb10c6a88488fca0090d18fdd61cd53c1e
pagrosi <107939587+pagrosi@users.noreply.github.com>
2023-02-27 18:47:27 +0200

    moved intRomanPairs to constants folder

--- src/components/IntToRoman/IntToRoman.tsx
+++ src/components/IntToRoman/IntToRoman.tsx
@@ -1,60 +1,6 @@
 import { useState } from 'react';
+import { intRomanPairs } from '@/constants/intRomanPairs';
 import { Input } from './Input';
-
-const intRomanPairs = [
-	{
-		int: 1000,
-		roman: 'M',
-	},
-	{
-		int: 900,
-		roman: 'CM',
-	},
-	{
-		int: 500,
-		roman: 'D',
-	},
-	{
-		int: 400,
-		roman: 'CD',
-	},
-	{
-		int: 100,
-		roman: 'C',
-	},
-	{
-		int: 90,
-		roman: 'XC',
-	},
-	{
-		int: 50,
-		roman: 'L',
-	},
-	{
-		int: 40,
-		roman: 'XL',
-	},
-	{
-		int: 10,
-		roman: 'X',
-	},
-	{
-		int: 9,
-		roman: 'IX',
-	},
-	{
-		int: 5,
-		roman: 'V',
-	},
-	{
-		int: 4,
-		roman: 'IV',
-	},
-	{
-		int: 1,
-		roman: 'I',
-	},
-];
 
 export const convertIntToRoman = (int: number): string => {
 	let result = '';
