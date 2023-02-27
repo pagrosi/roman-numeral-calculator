7a4d28d033af3627292b0081690aa2820ecb0a92
pagrosi <107939587+pagrosi@users.noreply.github.com>
2023-02-27 18:06:54 +0200

    added state for input, interface, connected files

--- src/components/IntToRoman/Input/Input.tsx
+++ src/components/IntToRoman/Input/Input.tsx
@@ -1,5 +1,19 @@
-export const Input = (): JSX.Element => (
+interface IProps {
+	value: number;
+	onChange: (value: number) => void;
+}
+
+export const Input = (props: IProps): JSX.Element => (
 	<label>
-		Integer Input: <input type="number" />
+		Integer Input:{' '}
+		<input
+			value={props.value}
+			onChange={(e) => {
+				props.onChange(e.target.valueAsNumber);
+			}}
+			type="number"
+			min={1}
+			max={1000}
+		/>
 	</label>
 );
--- src/components/IntToRoman/IntToRoman.tsx
+++ src/components/IntToRoman/IntToRoman.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { Input } from './Input';
 
 export const IntToRoman = (): JSX.Element => {
+	const [intInputNumber, setIntInputNumber] = useState(1);
 	return (
 		<>
-			<Input />
+			<Input value={intInputNumber} onChange={setIntInputNumber} />
 		</>
 	);
 };
